9881c63ee9abe9e07e2615f5ca33a6a33e7ff53a
Atamany <yigitataman1@gmail.com>
2024-12-19 13:02:51 +0300

    Proje dosyası ekle.

---
+++ BonusMvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BonusMvcStok.Models.Entity;
+
+namespace BonusMvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        DbMvcStokEntities db = new DbMvcStokEntities();
+        public ActionResult Index()
+        {
+            var kategoriler = db.TblKategori.ToList();
+            return View(kategoriler);
+        }
+    }
+}
